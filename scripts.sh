8309c907c441d3f71a38468256f4cd02150a6c55
patsailu <92512774+patsailu@users.noreply.github.com>
2022-09-05 03:01:05 +0530

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -4,3 +4,11 @@ apt-get upgrade -y
 apt-get install -y nginx
 apt-get install -y curl
 apt-get install -y git
+git config --global http.sslverify false
+apt-get -y install docker-compose
+chmod +x /usr/local/bin/docker-compose
+cd 
+curl -sSL https://bit.ly/2ysbOFE | bash -s
+curl -OL https://golang.org/dl/go1.18.2.linux-amd64.tar.gz
+sha256sum go1.18.2.linux-amd64.tar.gz
+tar -C /usr/local -xvf go1.18.2.linux-amd64.tar.gz
