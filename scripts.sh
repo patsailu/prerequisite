c7e61dcb0f808688038f3ad2e94c2a4888669704
patsailu <92512774+patsailu@users.noreply.github.com>
2022-09-05 10:33:12 +0530

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -4,6 +4,6 @@ apt-get upgrade -y
 apt-get install -y nginx
 apt-get install -y curl
 apt-get install -y git
-curl -L "https://github.com/docker/compose/releases/download/2.10.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/v2.10.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-curl -sSL https://bit.ly/2ysbOFE | bash -s
+curl -sSL https://raw.githubusercontent.com/hyperledger/fabric/main/scripts/bootstrap.sh| bash -s
