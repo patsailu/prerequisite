e1544792bc487b8eefdf5074871bed08d5b407c6
patsailu <92512774+patsailu@users.noreply.github.com>
2022-09-05 11:07:22 +0530

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -5,7 +5,7 @@ apt-get install -y nginx
 apt-get install -y curl
 apt-get install -y git
 apt-get update
-apt-get install docker.io
+apt-get install -y docker.io
 curl -L "https://github.com/docker/compose/releases/download/v2.10.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 cd ~
