491243668e2997bef128ae10b4538aa583f9f3f6
patsailu <92512774+patsailu@users.noreply.github.com>
2022-09-05 12:41:54 +0530

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -4,11 +4,13 @@ sudo apt-get upgrade -y
 sudo apt-get install -y nginx
 sudo apt-get install -y curl
 sudo apt-get install -y git
-sudo apt-get -y install docker-compose
+sudo apt install gnome-terminal
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg lsb-release docker.io docker-compose -y
 sudo systemctl start docker
 sudo systemctl enable docker
-user=$(whoami)
-sudo usermod -a -G docker $user
+sudo gpasswd -a $USER docker
+sudo setfacl -m "user:$USER:rw" /var/run/docker.sock
 curl -OL https://golang.org/dl/go1.18.2.linux-amd64.tar.gz
 sha256sum go1.18.2.linux-amd64.tar.gz
 sudo tar -C /usr/local -xvf go1.18.2.linux-amd64.tar.gz
