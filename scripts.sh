dedaa44c8f2593a0c63d38fe242428c5e958d8ef
patsailu <92512774+patsailu@users.noreply.github.com>
2022-09-07 09:51:44 +0530

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -8,6 +8,7 @@ sudo apt install gnome-terminal
 sudo apt-get update
 sudo apt-get install ca-certificates curl gnupg lsb-release docker.io -y
 curl -L https://github.com/docker/compose/releases/download/v2.6.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo gpasswd -a $USER docker
@@ -18,7 +19,7 @@ sudo snap install --classic code
 sudo apt install nodejs -y
 sudo apt install build-essential -y
 cd ~
-curl -sSL https://raw.githubusercontent.com/hyperledger/fabric/main/scripts/bootstrap.sh| bash -s
+curl -sSL https://raw.githubusercontent.com/hyperledger/fabric/main/scripts/bootstrap.sh| bash -s -o $HOME
 sudo apt-get -y install xfce4
 sudo apt-get -y install xrdp
 sudo systemctl enable xrdp
